5e28f2eddfefb89b74e970603ed3268f2a8aa2d7
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2020-11-11 11:51:47 +0100

    Update Cloudtopolis.sh

--- Cloudtopolis.sh
+++ Cloudtopolis.sh
@@ -134,5 +134,6 @@ echo -e "\e[36;1m[i] Cloudtopolis is running!"
 echo -e "\e[0;1mPress \e[35;1mCTRL+C \e[0;1mto stop.."
 echo -e "\e[0m"
 
+sudo chown -R 33:33 Cloudtopolis/inc Cloudtopolis/import Cloudtopolis/files
 trap 'docker rm -f $(docker container ls -q -a) > /dev/null 2>&1 ; echo ; exit' SIGINT SIGTERM EXIT
-while true ; do sudo chmod -R 777 Cloudtopolis/* ; sleep 3 ; done
+while true ; do sleep 3600 ; done
